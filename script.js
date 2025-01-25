e6a1c429b92ebb8fab97badfe41e62218e2f8f29
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-25 16:55:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+
+document.getElementById("btn").addEventListener("click", (e)=> {
+	e.preventDefault()
+	
+	let age = document.getElementById("age").value;
+	let name = document.getElementById("name").value;
+
+	if(!age || !name) {
+		alert("inputs cannot be empty");
+	}
+	
+	let voter_age = parseInt(age);
+	let voter_name = name;
+
+	let promise = new Promise((resolve, reject) => {
+		setTimeout(()=>{
+			if(voter_age > 18) {
+				resolve(`Welcome, ${voter_name}. You can vote.`)
+			}else {
+				reject(`Oh sorry ${voter_name}. You aren't old enough.`)
+			}
+		}, 4000)
+	});
+
+
+	promise.then((message)=> {
+		alert(message);
+	})
+	.catch((err) => {
+		alert(err);
+	});
+});
